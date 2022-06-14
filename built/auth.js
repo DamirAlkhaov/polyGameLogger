9820fcb184b9d754ca55e64b6d67e5c920ba4e64
Damir Alkhaov <alkhaovdamirgt@gmail.com>
2022-06-14 19:05:31 +0300

    0.1.1 Small Fix

--- built/auth.js
+++ built/auth.js
@@ -1,5 +1,6 @@
 export async function auth(polyId, ip) {
-    if (polyId && ip == "64.225.60.112") {
+    console.log(ip);
+    if (polyId && ip === "64.225.60.112") {
         return true;
     }
     else {
--- built/server.js
+++ built/server.js
@@ -19,7 +19,7 @@ app.post("/", async (req, res) => {
     if (req.query.key != process.env.SECRET_KEY) {
         return res.status(400).send("Failure : API Key isn't correct");
     }
-    const ip = req.connection.remoteAddress;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const id = req.query.id;
     const username = req.query.username;
     const color = req.query.color;
@@ -39,7 +39,7 @@ app.post("/msg", async (req, res) => {
     if (req.query.key != process.env.SECRET_KEY) {
         return res.status(400).send("Failure : API Key isn't correct");
     }
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const id = req.query.id;
     const username = req.query.username;
     const msg = req.query.msg;
